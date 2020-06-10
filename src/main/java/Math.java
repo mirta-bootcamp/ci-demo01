0b2308b240f7900ff25f7e4de63cd89310a9c651
Mirta Arambulo <arambulomirta@gmail.com>
2020-06-10 08:17:38 -0400

    Initial version of the project

---
+++ src/main/java/Math.java
@@ -0,0 +1,14 @@
+
+public class Math {
+	
+	/**
+	 * Sum the numbers
+	 * @param a the first number
+	 * @param b the second number
+	 * @return the sum as integer
+	 */
+	public static  int sum(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/test/java/MathTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathTest {
+
+	@Test
+	void testSum() {
+		assertEquals(3, Math.sum(1, 2));
+	}
+
+}
